397142a47fac633c757d51c0541de45f38ee8f1c
Darshan Shaligram <scintilla@gmail.com>
2014-12-08 20:36:19 -0500

    Add handy command-line test.

---
+++ websearch/google/cli/google.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/bevly/bevly/websearch/google"
+)
+
+func main() {
+	search := strings.Join(os.Args[1:], " ")
+	res, err := google.DefaultSearch().Search(search)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Search for %s failed: %s\n", search, err)
+		os.Exit(1)
+	}
+	fmt.Println("Found", len(res), "results for", search)
+	for i, r := range res {
+		fmt.Printf("%d) %s (%s)\n", i, r.Text, r.URL)
+	}
+}
